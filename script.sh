46950029498b6209b6883f75258a55e140eb787f
VladUshakevich <vladislav.ushakevich@gmail.com>
2021-11-09 12:32:38 +0300

    script

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir script_1;
+cd script_1;
+mkdir papka papka_2 papka_3 ;
+cd papka ;
+touch one.txt two.txt free.txt jeden.json dwa.json ;
+mkdir dota cs wow ;
+ls -la ;
+mv dota cs wow ../papka_2
+
